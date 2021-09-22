6d278728bf44fac112386f07a33bac6b903a3725
João Pedro Lescano <joaopedrolescano@gmail.com>
2021-09-22 18:48:05 -0400

    Primeiro commit

---
+++ imc.rb
@@ -0,0 +1,11 @@
+print 'Qual o seu peso?    '
+
+peso = gets.chomp.to_i
+
+print 'Qual sua altura? (Em metros)    '
+
+altura = gets.chomp.to_f
+
+imc = peso / (altura ** 2)
+
+print "Seu IMC é de #{imc}"
